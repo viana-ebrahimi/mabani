588d448cbd415f85ce7862e6b9bf7ad357f782dc
viana-ebrahimi <viona23ebrahimi@gmail.com>
2025-02-03 02:15:37 +0330

    add changeprice function

--- proje mabani.cpp
+++ proje mabani.cpp
@@ -1,99 +1,179 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
-using namespace std ;
-  
-  struct anbar { 
-              User people[5];
-              
-         creatusers ( people ) ;
-         
-          Item * itemlist = new Item [];
-          
-  } ;
-  
-struct User { 
-        string name;
+struct User
+{
+        string username;
         string password;
         string type;
-} ;
+};
 
-struct Item { 
-       string name ;
-       double price ;
-       
-} ;
- 
+struct Item
+{
+        string name;
+        double price;
+};
 
-void adminMenu(){ 
-         cout<<"  "
-}
-void creatusers (User people[]) { 
+struct myAnbarData
+{
+        const int numberOfPeople = 5;
+        User people[5];
+        Item *itemsList = nullptr;
+};
 
-        people[0] =  { "ali","1234","admin" }  ;
-        people[1] =  { "nava","4321","admin" } ;
-        people[2] =  { "user","7777","user" }  ;
-        people[3] =  { "user", "4444","user" } ;
-	    people[4] =  { "user","5555","user" }  ;
-  
-}
+void showUserMenu();
+void showAdminMenu();
+void createUsers(User people[]);
+void login(User people[], int size);
+void addItem(const string &item, int price);
 
-void login ( User people[] , int size ) { 
-	
- 
 
-             string inputN ;
-             string inputP ;
-             string type ;
-  
-     cout<< " enter your name: " <<endl<< " *if you are admin enter youre name , and if you are user enter user * " <<endl ;
-      cin>> inputN ;
-     cout<< " enter youre password:"<<endl ;
-      cin>> inputP ;
-     
-              bool login = false ;
-     for ( int i=0 ; i<5 ; i++ ){ 
-         if ( inputN == people[i].name && inputP == people[i].password ) { 
-            login = true ;
-            type = people[i].type ;
-           break;
-            
-		 }
-	 } 
-	 
-	     if (login == true ) { 
-        	cout<<" YOU ARE "<< type << endl; 
-        	
-        	if (type == "admin"){ 
-        	   adminMenu();
-        	    
-			}
-	
-	 } 
-	     else { 
-	       cout<<" Failed login" << endl;
-	 }
-   
+
+void addItem(const string &item, int price)
+{
+        cout << "item : " << item << "price: " << price << "added " << endl;
+        //todo
 } 
-void menu () { 
-        int choice ;
-        cout<< " welcome !  to login press 1 ,  to exit press other key . " <<endl;
-         cin>> choice;
-     
-       if (choice == 1) {  
-         login ( people , 5) ;
-         
-	 } 
-	   else { 
-	      cout<<"goodbye!"<<endl;
-	 }
-} 
- 
-
-int main () { 
+void removeItem(const string &item, int price){ 
+            cout << "item : " << item << "price: " << price << "removed " << endl;
+//todo
+}
+void changePrice(int price , int newPrice){
+	cout<< "price" << price << "updated to" <<newPrice << endl;
+	//todo
+}
+void wallet(int price , string name){
+	       	cout<< "wallet " << name << increase << " increased"  << endl;
+	       	//todo
+}
+void showAdminMenu()
+{
+        cout << "\nList of all admin commands:\n";
+        cout << "  add        \n";
+        cout << "  remove     \n";
+        cout << "  rename     \n";
+        cout << "  price      \n";
+        cout << "  credit     \n";
+        cout << "Enter command: " << endl;
+        string command;
+        cin >> command;
         
-         menu();
-         
-       
-
+        if (command == "add")
+        {
+                string item;
+                cin >> item;
+                int price;
+                cin >> price;
+                addItem(item, price);
+        }
+        else if (command == "remove")
+        {
+                string item;
+                cin >> item;
+                int price;
+                cin >> price;
+                removeItem(item, price);
+        } 
+        else if( command == "price"){
+              int price , int newPrice;
+              cin>>price;
+              cin>>newprice;
+              changePrice(price,newPrice);
+		} 
+		 
+		 else if(command == "credit"){ 
+		       int increase , string name;
+		       cin>>increase;
+		       cin>>name;
+		       wallet(increase , name );
+		 }
+        else
+        {
+                cout << "command not found" << endl;
+        }
 }
 
+void showUserMenu(){ 
+             cout << "\nList of all admin commands:\n";
+        cout << "  show item      \n";
+        cout << "  buy item     \n";
+        cout << "  balance    \n";
+        cout << "  help    \n";
+    
+        cout << "Enter command: " << endl;
+        string command;
+        cin >> command;
+            if(command == "show item"){ 
+               for(int i=0 ; i< size ; i++){
+               cout << itemeslist[i] << endl; 
+               
+			   } 
+			   string name;
+			   cin>> name;
+			   if( itemeslist[i] == name){
+			   	cout<< itemeslist
+			   }
+			}
+}
+
+void createUsers(User people[])
+{
+        people[0] = {"ali", "1234", "admin"};
+        people[1] = {"nava", "4321", "admin"};
+        people[2] = {"user1", "7777", "user"};
+        people[3] = {"user2", "4444", "user"};
+        people[4] = {"user3", "5555", "user"};
+}
+
+void login(User people[], int size)
+{
+        string inputN;
+        string inputP;
+        string type;
+        cout << "Enter your username: " << endl;
+        cout << "*If you are admin enter your name, and if you are user enter 'user'*" << endl;
+        cin >> inputN;
+        cout << "Enter your password:" << endl;
+        cin >> inputP;
+        bool loggedIn = false;
+        for (int i = 0; i < size; i++)
+        {
+                if (inputN == people[i].username && inputP == people[i].password)
+                {
+                        loggedIn = true;
+                        type = people[i].type;
+                        break;
+                }
+        }
+        if (loggedIn)
+        {
+                cout << "YOU ARE " << type << endl;
+                if (type == "admin")
+                {
+                        showAdminMenu();
+                }
+                else if (type == "user")
+                {
+                        showUserMenu();
+                }
+        }
+        else
+        {
+                cout << "Failed login" << endl;
+        }
+}
+
+void startProgram()
+{
+        const int numberOfUsers = 5;
+        myAnbarData anbar;
+        createUsers(anbar.people);
+        login(anbar.people, numberOfUsers);
+}
+
+int main()
+{
+        startProgram();
+        return 0;
+}
